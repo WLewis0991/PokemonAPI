65fe632d92e673b7fe27cae5599fc0e36ccf53ab
wlewis0991 <wlewis1990@gmail.com>
2026-02-07 18:44:56 -0800

    Project moved to new file

---
+++ index.js
@@ -0,0 +1,44 @@
+//Pokemon API
+
+const img = document.querySelector("img");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const output = document.querySelector(".output");
+const pokeNumber = document.querySelector(".pokeNumber");
+const type = document.querySelector(".type");
+//Event Listener
+button.addEventListener("click", () => {
+  const pokemon = input.value.trim().toLowerCase();
+  //Error for empty input
+  if (!pokemon) {
+    pokeNumber.textContent = "";
+    type.textContent = "";
+    img.src = "imgs/questionMark.svg";
+    output.textContent = "TRY AGAIN";
+    return;
+  }
+  //Fetching of pokemon API
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Not found");
+      }
+      return response.json();
+    })
+    //Data from API
+    .then((data) => {
+      pokeNumber.textContent = `#: ${data.id}`;
+      type.textContent = `Type: ${data.types[0].type.name}`;
+      img.src = data.sprites.front_default;
+      output.textContent = data.name.toUpperCase();
+    })
+    //Error if pokemon is not found
+    .catch(() => {
+      pokeNumber.textContent = "";
+      type.textContent = "";
+      img.src = "imgs/questionMark.svg";
+      output.textContent = "TRY AGAIN";
+    });
+  //Clear input
+  input.value = "";
+});
